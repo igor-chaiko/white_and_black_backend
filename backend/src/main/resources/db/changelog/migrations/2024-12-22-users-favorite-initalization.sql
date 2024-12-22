2ad01a3699fba2a0ba79f641c50507678410736e
Igor Chaiko <i.chaiko@g.nsu.ru>
2024-12-22 15:22:12 +0700

    .

--- backend/src/main/resources/db/changelog/migrations/2024-12-22-users-favorite-initalization.sql
+++ backend/src/main/resources/db/changelog/migrations/2024-12-22-users-favorite-initalization.sql
@@ -3,9 +3,10 @@
 --changeset igor-chaiko:5
 CREATE TABLE users_favorite
 (
-    id            BIGSERIAL   PRIMARY KEY,
-    entity_id     BIGINT      NOT NULL,
-    entity_type   TEXT        NOT NULL
+    id            BIGSERIAL                   PRIMARY KEY,
+    user_id       BIGINT REFERENCES users(id) NOT NULL,
+    entity_id     BIGINT                      NOT NULL,
+    entity_type   TEXT                        NOT NULL
 );
 
 COMMENT ON TABLE users_favorite IS 'Сущности, которые пользователь добавил в избраннле';
