a5b608d505ce14081f099b3476243648cdc5a1bd
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2025-01-06 17:17:22 +0700

    added validation for product review dto

--- backend/src/main/kotlin/black_and_white/product/review/model/dto/request/ProductReviewRequestDto.kt
+++ backend/src/main/kotlin/black_and_white/product/review/model/dto/request/ProductReviewRequestDto.kt
@@ -1,7 +1,14 @@
 package black_and_white.product.review.model.dto.request
 
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.Positive
+
 data class ProductReviewRequestDto(
+    @field:Positive
     val productId: Long,
     val review: String,
+    @field:Max(value = 5, message = "Оценка должна быть в диапазоне от 0 до 5")
+    @field:Min(value = 0, message = "Оценка должна быть в диапазоне от 0 до 5")
     val score: Int,
 )
